2d7a8e5ade707c01454781c162eab15a2ca56a36
joyalljijo <joyal2405@gmail.com>
2024-04-20 14:59:45 +0530

    Improved app's and csv file's ui a bit

--- backEnd/detector.py
+++ backEnd/detector.py
@@ -1,51 +1,8 @@
-# from typing import List, Tuple
-# from pathlib import Path
-# import pickle
-# import face_recognition
-# from collections import Counter
-# from PIL import Image, ImageDraw
-
-# DEFAULT_ENCODINGS_PATH = Path("output/ecncodings.pkl")
-
-# Path("training").mkdir(exist_ok=True)
-# Path("output").mkdir(exist_ok=True)
-# Path("validation").mkdir(exist_ok=True)
-
-# BOUNDING_BOX_COLOR = "blue"
-# TEXT_COLOR = "white"
-
-
-# def encode_known_faces(
-#         model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH
-# ) -> None:
-#     names = []
-#     encodings = []
-#     for filepath in Path("training").glob("*/*"):
-
-#         if not filepath.is_file() or filepath.suffix.lower() not in ['.jpg', '.jpeg', '.png']:
-#             continue
-
-#         name = filepath.parent.name
-#         image = face_recognition.load_image_file(filepath)
-
-#         face_locations = face_recognition.face_locations(image, model=model)
-#         face_encodings = face_recognition.face_encodings(image, face_locations)
-
-#         for encoding in face_encodings:
-#             names.append(name)
-#             encodings.append(encoding)
-
-#     name_encodings = {"names": names, "encodings": encodings}
-#     with encodings_location.open(mode="wb") as f:
-#         pickle.dump(name_encodings, f)
-
-
-from typing import List, Tuple
+from typing import List
 from pathlib import Path
 import pickle
 import face_recognition
 from collections import Counter
-from PIL import Image, ImageDraw
 
 DEFAULT_ENCODINGS_PATH = Path("output/ecncodings.pkl")
 
